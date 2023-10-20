0c3bac4effd230ab9294f22c3efa0af6bfe1a664
outpost-caprice <itani@outpost-corp.com>
2023-10-20 16:42:37 +0900

    modified:   web_searcher.py firefox to chrome

--- web_searcher.py
+++ web_searcher.py
@@ -4,12 +4,16 @@ import os
 
 class WebSearcher:
   def __init__(self):
-    self.api_key = os.getenv("GOOGLE_API_KEY")
-    self.cse_id = os.getenv("GOOGLE_CSE_ID")
-    self.driver = webdriver.Firefox()
+    self.api_key = os.environ['GOOGLE_API_KEY']
+    self.cse_id = os.environ['GOOGLE_CSE_ID']
+    
+    # ChromeDriverの場所を指定
+    driver_path = os.environ["DRIVER_PATH"]
+    # FirefoxからChromeに変更
+    self.driver = webdriver.Chrome(executable_path=driver_path)
 
   def search(self, query, num_results=10):
-    service = build("customsearch", "v1", developerKey=self.api_key)  
+    service = build("customsearch", "v1", developerKey=self.api_key)
     result = service.cse().list(q=query, cx=self.cse_id, num=num_results).execute()
     return [item['link'] for item in result['items']]
 
