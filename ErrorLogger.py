1e4e6a69e91117dc2921de67d30b85adf2fe222b
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-10-23 11:41:04 +0900

    変更
    エラーログを書き出すようにして、後方互換性を持たせた。
    次から修正を行う際はエラーレベルを引数に入れること

--- ErrorLogger.py
+++ ErrorLogger.py
@@ -1,11 +1,29 @@
 import logging
+import datetime
 
 class ErrorLogger:
 
-  def __init__(self, log_file):
-    self.logger = logging.getLogger(__name__)
-    handler = logging.FileHandler(log_file)
-    self.logger.addHandler(handler)
+    def __init__(self, log_file=None):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.ERROR)
+        
+        if log_file is None:
+            log_file = f"errors_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.log"
+        
+        handler = logging.FileHandler(log_file)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        
+        self.logger.addHandler(handler)
 
-  def log(self, error):
-    self.logger.error(error)
+    def log(self, error, level='error'):
+        if level == 'error':
+            self.logger.error(error)
+        elif level == 'warning':
+            self.logger.warning(error)
+        elif level == 'info':
+            self.logger.info(error)
+        elif level == 'debug':
+            self.logger.debug(error)
+        else:
+            self.logger.error(f"Unknown log level: {level}. Original error: {error}")
