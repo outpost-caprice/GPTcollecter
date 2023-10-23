2f6cc5b407e86b7127b7947e5e6043e41e35d30a
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-10-23 13:08:09 +0900

    FileManager.py の更新
    エラーログ等の修正。

--- FileManager.py
+++ FileManager.py
@@ -1,22 +1,37 @@
 import os
 from pathlib import Path
 from zipfile import ZipFile
+from error_log import ErrorLogger
 
 class FileManager:
 
-  def __init__(self, output_dir):
-    self.output_dir = Path(output_dir)
+    def __init__(self, output_dir, log_file="file_manager_errors.log"):
+        self.output_dir = Path(output_dir)
+        self.logger = ErrorLogger(log_file)
+        self._create_output_dir()
 
-  def save_summary(self, text, url):
-    filename = self._get_filename(url)
-    file_path = self.output_dir / filename
-    with open(file_path, 'w') as f:
-      f.write(text)
+    def _create_output_dir(self):
+        try:
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            self.logger.log(f"出力ディレクトリの作成中にエラーが発生しました: {e}")
 
-  def _get_filename(self, url):
-    return url.replace('/', '_') + '.txt'
+    def save_summary(self, text, url):
+        try:
+            filename = self._get_filename(url)
+            file_path = self.output_dir / filename
+            with open(file_path, 'w') as f:
+                f.write(text)
+        except Exception as e:
+            self.logger.log(f"サマリーの保存中にエラーが発生しました: {e}")
 
-  def make_zipfile(self, name):
-    with ZipFile(name, 'w') as zip:
-      for path in self.output_dir.glob('*'):
-        zip.write(path, path.name)
+    def _get_filename(self, url):
+        return url.replace('/', '_') + '.txt'
+
+    def make_zipfile(self, name):
+        try:
+            with ZipFile(name, 'w') as zip:
+                for path in self.output_dir.glob('*'):
+                    zip.write(path, path.name)
+        except Exception as e:
+            self.logger.log(f"ZIPファイルの作成中にエラーが発生しました: {e}")
