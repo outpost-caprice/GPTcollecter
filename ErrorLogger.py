95516d4673f5fe650511d809df063f04777a2126
outpost-caprice <itani@outpost-corp.com>
2023-10-30 17:50:15 +0900

    modified:   ErrorLogger.py 	deleted:    main_errors.log

--- ErrorLogger.py
+++ ErrorLogger.py
@@ -1,17 +1,17 @@
-from enum import Enum
 import logging
+from enum import Enum
 import datetime
 
-class LogLevel(Enum):
+class LogLevel(Enum):  
   DEBUG = 10
-  INFO = 20 
+  INFO = 20
   WARNING = 30
   ERROR = 40
 
 class ErrorLogger:
 
   def __init__(self, log_file=None):
-
+    # ログファイル名の自動設定 
     if log_file is None:
       now = datetime.datetime.now()
       log_file = now.strftime("errors_%Y%m%d_%H%M%S.log")
@@ -19,25 +19,19 @@ class ErrorLogger:
     self.logger = logging.getLogger(__name__)
     self.logger.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+    # loggingモジュールのフォーマッタを使用
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') 
 
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
     self.logger.addHandler(file_handler)
 
-  def log(self, message, level=LogLevel.ERROR):
-
-    if level == LogLevel.DEBUG:
-      self.logger.debug(message)
-
-    elif level == LogLevel.INFO:
-      self.logger.info(message)
-
-    elif level == LogLevel.WARNING:
-      self.logger.warning(message)
-
-    elif level == LogLevel.ERROR:
-      self.logger.error(message)
-
-    else:
-      self.logger.error(f"Unknown log level: {level}")
+  def log(self, message, level=LogLevel.INFO):
+    # dictでlevelの条件分岐を置き換え
+    level_map = {
+      LogLevel.DEBUG: self.logger.debug,
+      LogLevel.INFO: self.logger.info,
+      LogLevel.WARNING: self.logger.warning,
+      LogLevel.ERROR: self.logger.error
+    }
+    level_map[level](message)
